cb18a7f57ad2a32b375b4b9bb8f5ab18c4b3c6dd
Rafael Matera <rmaterapay@gmail.com>
2025-07-08 14:08:01 -0600

    Desktop sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,13 @@
 import "../styles/styles.css"
 import MobileMenu from "./modules/MobileMenu"
+import RevealOnScroll from "./modules/RevealOnScroll"
+import StickyHeader from "./modules/StickyHeader"
+
+const stickyHeader = new StickyHeader()
+const mobileMenu = new MobileMenu()
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 
 if (module.hot) {
   module.hot.accept()
 }
-
-const mobileMenu = new MobileMenu()
