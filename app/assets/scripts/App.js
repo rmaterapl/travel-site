28a49cb8a6e54b1d9509f9d14f8bd1d39ee61c8f
Rafael Matera <rmaterapay@gmail.com>
2025-07-08 21:42:42 -0600

    Modal completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,10 +3,29 @@ import MobileMenu from "./modules/MobileMenu"
 import RevealOnScroll from "./modules/RevealOnScroll"
 import StickyHeader from "./modules/StickyHeader"
 
-const stickyHeader = new StickyHeader()
-const mobileMenu = new MobileMenu()
+new StickyHeader()
+new MobileMenu()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+let modal
+
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ "./modules/Modal")
+        .then(x => {
+          modal = new x.default()
+          setTimeout(() => modal.openModal(), 20)
+        })
+        .catch(() => {
+          console.log("There was a problem")
+        })
+    } else {
+      modal.openModal()
+    }
+  })
+})
 
 if (module.hot) {
   module.hot.accept()
